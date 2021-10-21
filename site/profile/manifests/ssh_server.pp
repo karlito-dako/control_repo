336a45203fc6f71dcf4e9b451e08261180e70e0a
Karl Correia <60675321+karlito-dako@users.noreply.github.com>
2021-10-21 18:57:27 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQChnYk08dCOANgZ3hKXPffXMK++bsCTrnwKOxFNMbF3wqkxlYxD4cDHkHnxXW/+7mt8LP0LH/rXHWt69aD4rss6xE/lPZsZbbgS+9eoEW+dSje3Y7inIjKHV4FqDnPKhDl+fCOP/ftjt3c3KhzoegPRYuHAKxuofk4OFYUKrgDEF93yPfxaTFZbTjsst3Z/XchvTM6Qocs9q0fHJjc9Sq3zk8tEfzpAyG7owbR180zd5ZqfVnR+LOvjsxUXyzZIyW9+H3IMVBfwDZwXyiHHYgqqAgtuerfuxaxUELP8v9w6e9rDZ6wN1DKN2QxpUBCsctrX76EoB6XklCrFbGJ02q2X',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQChnYk08dCOANgZ3hKXPffXMK++bsCTrnwKOxFNMbF3wqkxlYxD4cDHkHnxXW/+7mt8LP0LH/rXHWt69aD4rss6xE/lPZsZbbgS+9eoEW+dSje3Y7inIjKHV4FqDnPKhDl+fCOP/ftjt3c3KhzoegPRYuHAKxuofk4OFYUKrgDEF93yPfxaTFZbTjsst3Z/XchvTM6Qocs9q0fHJjc9Sq3zk8tEfzpAyG7owbR180zd5ZqfVnR+LOvjsxUXyzZIyW9+H3IMVBfwDZwXyiHHYgqqAgtuerfuxaxUELP8v9w6e9rDZ6wN1DKN2QxpUBCsctrX76EoB6XklCrFbGJ02q2X',
 	}  
 }
